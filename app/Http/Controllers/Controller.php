7fee46a840a1aaf0283031d2cea0d1a2e6b02343
黄思盛 <einsition@huangsishengdeMacBook-Pro.local>
2019-09-06 11:54:12 +0800

    移除项目中laravel-china镜像

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -8,6 +8,11 @@ use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
+/**
+ * Class Controller
+ * @property \Illuminate\Auth\GenericUser|\App\Models\User $user
+ * @package App\Http\Controllers
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests, Helpers;
